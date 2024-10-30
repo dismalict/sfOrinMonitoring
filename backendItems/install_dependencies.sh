2066356b8f908b778cbb3382baf2210f62a61c50
dismalict <dismal.ict@outlook.com>
2024-10-30 13:42:40 -0500

    Update 1.66

--- backendItems/install_dependencies.sh
+++ backendItems/install_dependencies.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+# Check if the script is run as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root or with sudo"
+  exit
+fi
+
 # Update package list
+echo "Updating package list..."
 apt-get update
 
 # Install pip if not installed
-if ! command -v pip &> /dev/null
-then
+if ! command -v pip &> /dev/null; then
     echo "pip not found, installing..."
     apt-get install -y python3-pip
 fi
 
 # Install required Python packages
-sudo pip3 install --upgrade mysql-connector-python jetson_stats psutil
-
-# Ensure jtop is installed
-sudo apt install jtop
+pip3 install --upgrade \
+    mysql-connector-python \
+    jetson-stats \
+    psutil \
+    configparser \
+    subprocess32
+    
+# Ensure other packages is installed
+apt-get install -y nano
